9b590dca80167ba20335ee3e4edd6cf89f97e383
Raman <33904740+Rmnlly@users.noreply.github.com>
2018-04-04 21:45:05 -0400

    Added video for mural that plays on hover

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
 window.addEventListener("DOMContentLoaded", function(){
+    let muralVideo = document.querySelector(".video1");
+
+
+    function pause() {
+        muralVideo.pause();
+    }
+
+    function play() {
+        if (muralVideo.paused) {
+            muralVideo.play();
+        }
+    }
+
 
 
 });
